56caa812d48b0a5baf79e4087fdc5c68d25fae30
vivisecret <159822879+vivisecret@users.noreply.github.com>
2024-02-12 16:08:17 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+let button = document.getElementById.id('no');
+let height = window.innerHeight - 50;
+let width = window.innerWidth -50;
+
+button.addEventListener('mouseover', function ( ) {
+    button.style.position = "absolute";
+    button.style.top = Math.random( ) * height + "px";
+    button.style.left = Math.random( ) * width + "px";
+
+})
+     
+
